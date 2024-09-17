187dc3560ba00508e12085d06b50866e719c379b
ayush2905 <ayushagrawal2905@gmail.com>
2024-09-18 05:03:04 +0530

    throttle functionality in text box

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ const debounceText = document.getElementById("debounce");
 const throttleText = document.getElementById("throttle");
 
 const updateDebounceText = debounce(text => {
-    // console.log("hello")
     debounceText.textContent = text;
+})
+
+const updateThrottleText = throttle(text => {
+    throttleText.textContent = text;
 })
 
 input.addEventListener("input", e => {
     defaultText.textContent = e.target.value;
     updateDebounceText(e.target.value);
+    updateThrottleText(e.target.value);
 })
 
 
-// input.addEventListener("input", e => updateDebounceText(e.target.value))
 
-
-
+// batch all the changes and send them at once instead of sending multiple requests
 function debounce(cb, delay = 1000) {
     let timeout;
 
@@ -26,5 +28,33 @@ function debounce(cb, delay = 1000) {
         timeout = setTimeout(() => {
             cb(...args)
         }, delay)
+    }
+}
+
+function throttle(cb, delay = 1000) {
+    let shouldWait = false;
+    let waitingArgs;
+
+    const setTimeoutFunc = () => {
+        if(waitingArgs==null) {
+            shouldWait = false;
+        } else {
+            cb(...waitingArgs);
+            waitingArgs = null;
+            setTimeout(setTimeoutFunc, delay);
+        }
+    }
+
+    return (...args) => {
+        if(shouldWait) {
+            waitingArgs = args;
+            return;
+        }
+
+        cb(...args);
+
+        shouldWait = true;
+
+        setTimeout(setTimeoutFunc, delay);
     }
 }
